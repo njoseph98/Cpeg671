b6d75833870e4300d88a9eb3c76e49db0d440f7e
njoseph98 <38293469+njoseph98@users.noreply.github.com>
2021-09-14 13:06:45 -0400

    Lab2

---
+++ lab2-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int a=32;
+    int b=46;
+    int x=55;
+    x=a+b+x;
+    return x;
+    
+}
---
+++ lab2-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a=10;
+    int b=14; 
+    if (a>=b){
+        return 2;
+    }
+    return 1;
+}
